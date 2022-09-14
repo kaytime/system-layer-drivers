b7b32a613c90fb586c9fa5ac5e7036b8bd11bd94
Steph DGenius <stephanetse@gmail.com>
2022-09-14 15:28:14 +0100

    0.1.0+alpha

--- gh-build.sh
+++ gh-build.sh
@@ -4,17 +4,16 @@ apt-get --yes update
 apt-get --yes install wget equivs curl git
 
 deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
-git_commit=$(git rev-parse --short HEAD)
 
 printf >configuration "%s\n" \
 	"Section: admin" \
 	"Priority: required" \
 	"Homepage: https://kaytime.github.io" \
 	"Package: system-layer-drivers" \
-	"Version: $PROJECT_VERSION" \
+	"Version: 0.1.0+alpha" \
 	"Maintainer: Stephane Tsana <stephanetse@gmail.com>" \
 	"Depends: $deps" \
 	"Architecture: all" \
-	"Description: Driver layout metapackage for Kaytime."
+	"Description: Drivers layout metapackage for Kaytime."
 
 equivs-build configuration
